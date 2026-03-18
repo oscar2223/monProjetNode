71e5533af8ab45ed98a306072e59941e3eaa34f9
Oscar Da Silva <dasilosc@ur.univ-rouen.fr>
2026-03-18 11:27:23 +0100

    troisieme commit

--- serveur.js
+++ serveur.js
@@ -4,6 +4,10 @@ let port=3000;
 app.use ( express.static( __dirname + '/www') ) ;
 app.use ('/js' , express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use ('/css' , express.static( __dirname + '/node_modules/bootstrap/dist/css'));
+app.set ( 'view engine' , 'ejs');
+app.use ( '/views' , express.static( __dirname + '/views') ) ; // r e d i r e c t v i ew s
+
+
 
 app.listen(port,()=> {
     console.log('Le serveur est en route');
@@ -12,5 +16,4 @@ app.listen(port,()=> {
 
 
 app.get ( '/' , (req , res , next ) => {
-res.sendFile ('/www/index.html') ;
-});
+res.render ( 'index.ejs') ;});
